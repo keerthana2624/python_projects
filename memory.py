b0842e9802fdbdc9cd06b99d20b361105e9567d3
keerthana2624 <keerthanamummy2002@gmail.com>
2024-05-31 12:21:02 +0530

    generated board with set of paired cards

--- memory.py
+++ memory.py
@@ -1,1 +1,9 @@
 import random
+
+def generateBoard():
+    """Generates and returns a shuffled board with pairs of cards."""
+    cards = ["A", "C", "B", "D", "E", "F", "G", "H"]
+    totalcards = cards * 2
+    random.shuffle(totalcards)
+    return totalcards
+# print(generateBoard())

d457a6f791e5e3d0d1437ce370a2e84c186fc721
keerthana2624 <keerthanamummy2002@gmail.com>
2024-06-01 12:42:33 +0530

    I have implimented the function to load the collection from a JSON file and main function implimented

--- books.py
+++ books.py
@@ -33,8 +33,49 @@ def save_to_json(books, filename="books.json"):
         print(filename)
     except IOError as e:
         print(e)
-books = [
-    {"title": "Book One", "author": "Author One", "year": 2001, "genre": "Fiction"},
-    {"title": "Book Two", "author": "Author Two", "year": 2002, "genre": "Non-fiction"}
-]
+# books = [
+#     {"title": "Book One", "author": "Author One", "year": 2001, "genre": "Fiction"},
+#     {"title": "Book Two", "author": "Author Two", "year": 2002, "genre": "Non-fiction"}
+# ]
 # save_to_json(books)
+
+
+def load_from_json(filename="books.json"):
+    try:
+        if not os.path.exists(filename):
+            print(filename)
+            return []
+        with open(filename, "r") as file:
+            return json.load(file)
+    
+    except (IOError, json.JSONDecodeError) as e:
+        print(e)
+        return []
+# books = load_from_json()
+# print(books)
+
+def main():
+    books = load_from_json()
+    while True:
+        print("\n===== Personal Book Collection Management =====")
+        print("1. Add Book")
+        print("2. View Collection")
+        print("3. Save to JSON")
+        print("4. Load from JSON")
+        print("5. Exit")
+        choice = input("Enter your choice: ")
+        if choice == '1':
+            add_book(books) 
+        elif choice == '2':
+            view_collection(books)
+        elif choice == '3':
+            save_to_json(books)
+        elif choice == '4':
+            books = load_from_json()
+        elif choice == '5':
+            save_to_json(books)
+            print("Exiting program.")
+            break 
+        else:
+            print("Invalid choice. Please try again.")
+main()

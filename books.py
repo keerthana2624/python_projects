a00f62926b2117c72d0d6fff7d30196c1c6ab20b
keerthana2624 <keerthanamummy2002@gmail.com>
2024-06-01 12:28:28 +0530

    i have imported the json file and  implimented the function to save the collection to a JSON file

--- books.py
+++ books.py
@@ -1,3 +1,6 @@
+
+import json
+import os
 def add_book(books):
     title=input("Enter the title of the book: ")
     author=input("Enter the author of the book: ")
@@ -6,8 +9,8 @@ def add_book(books):
     book={"title":title, "author":author,"year":year,"genre":genre}
     books.append(book)
     print("book added successfully")
-books=[]
-add_book(books)
+# books=[]
+# add_book(books)
 
 def view_collection(books):
     if not books:
@@ -19,8 +22,19 @@ def view_collection(books):
             print("Year:", book['year'])
             print("Genre:", book['genre'])
             print()
-books=[]
-books=add_book(books)
-view_collection(books)
+# books=[]
+# books=add_book(books)
+# view_collection(books)
 
- 
+def save_to_json(books, filename="books.json"):
+    try:
+        with open(filename, "w") as file:
+            json.dump(books, file)
+        print(filename)
+    except IOError as e:
+        print(e)
+books = [
+    {"title": "Book One", "author": "Author One", "year": 2001, "genre": "Fiction"},
+    {"title": "Book Two", "author": "Author Two", "year": 2002, "genre": "Non-fiction"}
+]
+# save_to_json(books)

f87521d227254f43d5a53554209693d1c05d429e
keerthana2624 <keerthanamummy2002@gmail.com>
2024-06-06 12:06:53 +0530

    i have implimented the function for creatpost and viewtimeline

--- socialmedia.py
+++ socialmedia.py
@@ -3,6 +3,26 @@ def initializeplatform():
     return platform
 # print(initializeplatform())
 
+def creat_post(platform,content):
+        new_post={}
+        new_post["content"]=content
+        new_post["likes"]=0
+        new_post["comments"]=[]
+        platform.append(new_post)
+        return platform
+# platform=initializeplatform()
+# content="name"
+# print(creat_post(platform,content))
+
+def viewtimeline(platform):
+    for post in platform:
+        dic={"post_name":post["content"],
+              "no_of_likes":post["likes"],
+              "comments":post["comments"]}
+        print(dic)
+# platfrom=initializeplatform()
+# p=creat_post(platfrom,content="hello")
+# print(viewtimeline(p))
 
 
 

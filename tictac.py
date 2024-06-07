ef2c76349a63b75927d6b600eda47f2d0ee05857
keerthana2624 <keerthanamummy2002@gmail.com>
2024-06-07 21:47:50 +0530

    implimented the makemove and checkwin functions

--- tictac.py
+++ tictac.py
@@ -16,3 +16,27 @@ def displayBoard(board):
     print()
 # board=initializeBoard()
 # displayBoard(board)
+
+def makeMove(board, player, row, col):
+    if 0 <= row < 3 and 0 <= col < 3 and board[row][col] == ' ':
+        board[row][col] = player
+        return True
+    return False
+# board=initializeBoard()
+# player=1
+# row=0
+# col=0
+# print(makeMove(board,player,row,col))
+
+def checkWin(board):
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] != ' ':
+            return board[i][0]
+        if board[0][i] == board[1][i] == board[2][i] != ' ':
+            return board[0][i]
+    if board[0][0] == board[1][1] == board[2][2] != ' ':
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != ' ':
+        return board[0][2]
+    return None
+# print(checkWin(board))

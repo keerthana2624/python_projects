5be96ac36a1e141111ad44f2fd4393928bbf243c
keerthana2624 <keerthanamummy2002@gmail.com>
2024-06-10 10:15:28 +0530

    implimented the parseQuestions function

--- quiz.py
+++ quiz.py
@@ -7,3 +7,25 @@ def loadData(filename):
     return d
 x=loadData('questions.txt')
 
+
+
+def parseQuestions(data):
+    l=[]
+    lines=data.split("\n")
+    # print(lines)
+    for i in lines:
+        d={}
+        s=i.split(":")
+        # print(s)
+        d["question_text"]=s[0]
+        d["choices"]=s[1].split(",")
+        d["correct_option"]=int(s[2])
+        d["max_marks"]=int(s[3])
+        d["penalty"]=int(s[4])
+        l.append(d)
+    # print(l)  
+    return l 
+y=parseQuestions(x)
+
+
+

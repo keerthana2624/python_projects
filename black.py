798104c1786df6fe330da5836c642b00a5c6dee7
keerthana2624 <keerthanamummy2002@gmail.com>
2024-06-10 16:13:58 +0530

    implimentation of blackjack game is completed

--- black.py
+++ black.py
@@ -25,18 +25,20 @@ def calculate_score(cards):
 # cards=[1,4,2]
 # print(calculate_score(cards))   
 
-
 def play_game():
+    print("welcome to the blackjack game")
     deck = create_deck()
+    # print(deck)
     player_cards = [deal_card(deck), deal_card(deck)]
     dealer_cards = [deal_card(deck), deal_card(deck)]
     while True:
         player_score = calculate_score(player_cards)
+        # print(player_score)
         print(f"Your cards: {player_cards}, current score: {player_score}")
         if player_score > 21:
-            print("You went over. You lose 😭")
+            print("You are lose")
             return
-        player_choice = input("Type 'y' to get another card, 'n' to pass: ")
+        player_choice = input("Type 'y' get another card, 'n' to pass: ")
         if player_choice == 'y':
             player_cards.append(deal_card(deck))
         else:
@@ -44,12 +46,12 @@ def play_game():
     while calculate_score(dealer_cards) < 17:
         dealer_cards.append(deal_card(deck))
     dealer_score = calculate_score(dealer_cards)
+    # print(dealer_score)
     print(f"Dealer's cards: {dealer_cards}, dealer's score: {dealer_score}")
     if dealer_score > 21 or player_score > dealer_score:
-        print("You win 😃")
+        print("You are winner")
     elif player_score < dealer_score:
-        print("You lose 😤")
+        print("You are loser")
     else:
-        print("It's a draw 🤝")
-
-# play_game
+        print("It's a draw")
+play_game()
